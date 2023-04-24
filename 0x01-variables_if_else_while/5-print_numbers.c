07e2eb1d0bcb72f29ef19e98330c57f44aa3ec8c
Laurenmsoo <lmugwiria13@gmail.com>
2023-04-24 11:42:51 -0700

    Print 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lower case alphabets using putchar
+ *
+ * Return: 0  (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 0; x <= 9; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
