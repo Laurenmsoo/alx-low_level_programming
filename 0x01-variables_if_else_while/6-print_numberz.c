4ba68a3d494e60b6e3450869833a97afd536ddae
Laurenmsoo <lmugwiria13@gmail.com>
2023-04-24 11:55:43 -0700

    Print 0-10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lower case alphabets using putchar
+ *
+ * Return: 0  (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+putchar(x);
+}
+putchar ('\n');
+return (0);
+}
