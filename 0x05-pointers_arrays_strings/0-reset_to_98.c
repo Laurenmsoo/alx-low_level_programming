cc28f1c1b32781c1c9210fda96e058a1d0b77ee6
Laurenmsoo <lmugwiria13@gmail.com>
2023-05-02 12:40:29 -0700

    reset 402 to 98 using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98-takes a pointer to an int and changes the value to point to 98
+ * @n: input variable
+ * Return: n for sucess
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
