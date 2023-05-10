224fc7e22213375017724ca4138e8a0e5ecf7f55
Laurenmsoo <lmugwiria13@gmail.com>
2023-05-10 11:17:41 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string
+ * @s: string input
+ * Return: 0 Success
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
