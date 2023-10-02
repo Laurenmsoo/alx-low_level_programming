53f7095765bbcf13d87081e04228dd8e40e7c314
Laurenmsoo <lmugwiria13@gmail.com>
2023-10-02 12:56:30 -0700

    appenda text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fdef = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(fdef, text_content,slen);
+	wrt = write(fdef, text_content, slen);
 
 	if (fdef == -1 || wrt == -1)
 		return (-1);
