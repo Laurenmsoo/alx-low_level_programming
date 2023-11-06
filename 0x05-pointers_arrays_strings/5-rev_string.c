9028532b05a0f9f55dc131e48a679edf81fe00a7
Laurenmsoo <lmugwiria13@gmail.com>
2023-11-06 15:55:31 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,8 +2,23 @@
 /**
  * rev_string - reverses a string
  * @s: string to reverse
- */ 
+ */
 void rev_string(char *s)
 {
 	int i;
+	int len = 0;
+	int mid;
+	int temp;
 
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	mid = len / 2;
+	for (i = 0; i < mid; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
