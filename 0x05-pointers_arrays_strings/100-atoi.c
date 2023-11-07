cfc8f3b6a3ca1c6cd2e28be34ae6223005e237bb
Laurenmsoo <lmugwiria13@gmail.com>
2023-11-06 19:09:04 -0800

    crack password

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,11 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i, d, num, len, f, digit;
 
 	i = 0;
 	d = 0;
-	n = 0;
+	num = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
@@ -30,7 +30,7 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digit;
+			num = num * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
@@ -42,5 +42,5 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 
-	return (n);
+	return (num);
 }
