1800ae0048ea8ab799f4df00e1c54439e7f77664
Laurenmsoo <lmugwiria13@gmail.com>
2023-11-21 14:02:06 -0800

    frees

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -17,18 +17,3 @@ void free_grid(int **grid, int height)
 	}
 	free(grid);
 }
-_putchar.c
-#include "main.h"
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to be printed
- *
- * Return: 1 on success.
- * return: -1 on error and set the errono appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
