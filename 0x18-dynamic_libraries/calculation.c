42cd973b5cbbd12ca8c69671f9448e032bfd9bb7
Laurenmsoo <lmugwiria13@gmail.com>
2024-01-29 17:36:41 -0800

    c in python

---
+++ 0x18-dynamic_libraries/calculation.c
@@ -0,0 +1,50 @@
+/**
+ * add - adds 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substracts 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - finds the remainder of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
