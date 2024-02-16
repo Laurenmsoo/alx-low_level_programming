2d7315c56a5e4e2d93f246102fb4a709fe32fc66
Laurenmsoo <lmugwiria13@gmail.com>
2024-02-16 04:09:19 -0800

    create hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
         hash_table_t *htable;
         unsigned long int i;
 
-        ht_table = malloc(sizeof(hash_table_t));
+        htable = malloc(sizeof(hash_table_t));
         if (htable == NULL)
                 return (NULL);
 
